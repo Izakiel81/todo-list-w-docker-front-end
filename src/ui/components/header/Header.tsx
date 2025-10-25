3badd1cad4713b68e1d17efdc8cf6db188668052
Maksym Habinskyi <mgabin348@gmail.com>
2025-10-25 14:51:51 +0200

    adding header

---
+++ src/ui/components/header/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">My Tasks</h1>
+      <p className="header__subtitle">Stay organized and productive</p>
+    </div>
+  );
+};
